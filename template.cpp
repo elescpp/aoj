9943a0c07dee89b589396b8fab7177a40a067fb9
elescpp <elescppp@gmail.com>
2015-03-30 05:30:16 +0900

    improved template.cpp

--- template.cpp
+++ template.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
+#include <cstdio>
+
+#include <cmath>
+
+#include <array>
+#include <vector>
+#include <list>
+
+#include <utility>
+#include <functional>
+#include <algorithm>
 
 using namespace std;
+using namespace std::placeholders;
 
 int main(void)
 {
 
 	return 0;
 }
+
