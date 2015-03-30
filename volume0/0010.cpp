b8a77d0c2579cce1907e0d6f19f9d9bd2388609c
elescpp <elescppp@gmail.com>
2015-03-30 23:27:09 +0900

    といた

---
+++ volume0/0010.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdio>
+
+#include <cmath>
+
+#include <array>
+#include <vector>
+#include <list>
+
+#include <utility>
+#include <functional>
+#include <algorithm>
+
+using namespace std;
+using namespace std::placeholders;
+
+int main(void)
+{
+	array<double, 6> input;
+	for(double& d : input){
+		cin >> d;
+	}
+
+
+	return 0;
+}
+
